b96a4d5ea96f0b5857eef2e2740e7628a1005c2a
KTH <sauce0428@gmail.com>
2026-02-05 16:43:43 +0900

    [생성] async, await 비동기 처리방식

---
+++ section01/chapter01/chapter30.js
@@ -0,0 +1,31 @@
+//async, await = > promise 이다.
+async function getData(){
+  return {name: 'kdj', age: 30}
+}
+
+function getData2(){
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      let flag = true;
+      if (flag) {
+        resolve({name: 'kdj', age: 30})
+      } else {
+        reject("비동기처리 요청 실패")
+      }
+    }, 1000)
+  })
+}
+
+//  console.log(getData2())
+
+
+  async function printData(){
+    getData2().then((value)=> console.log(value))
+  }
+  
+  async function printData2(){
+    const data = await getData2()
+    console.log(data)
+  }
+
+  printData2();
