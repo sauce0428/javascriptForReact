c8d6be078ddb33ec9dba081be64cca7203d1165d
KTH <sauce0428@gmail.com>
2026-02-05 10:39:20 +0900

    [생성] JSON stringfy

---
+++ section01/chapter01/chapter27.js
@@ -0,0 +1,8 @@
+//1. 사용자 정의 객체
+let object1 = {name : "홍길동", age : 30}
+
+let object2 = {...object1}
+object2.hobby = "테니스";
+
+console.log(object2)
+console.log(JSON.stringify(object1) === JSON.stringify(object2))
