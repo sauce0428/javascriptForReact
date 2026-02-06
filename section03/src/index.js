9367671df5f86d488111592e6f3989507f507f1b
KTH <sauce0428@gmail.com>
2026-02-06 09:30:59 +0900

    [생성] 리액트 시작

---
+++ section03/src/index.js
@@ -0,0 +1,9 @@
+import add from './math.js';
+import {sub, div} from './math.js';
+import randomcolor from 'randomcolor';
+
+let color = randomcolor();
+console.log(`color = ${color}`);
+console.log(sub(10,20));
+console.log(div(10,20));
+console.log('안녕 node.js')
---
+++ section03/src/math.js
@@ -0,0 +1,13 @@
+//math 모듈 (객체: function 함수선언, 함수표현식, 함수화살표)
+//java : class
+export default function add(a, b){
+  return a+b;
+}
+
+export function sub(a, b){
+  return a-b;
+}
+
+export function div(a, b){
+  return a / b;
+}
