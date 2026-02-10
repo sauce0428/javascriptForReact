9bdc9d252ae1a7705c51aa03bbade5ba3d066940
KTH <sauce0428@gmail.com>
2026-02-10 14:21:24 +0900

    [단위프로젝트] todoList 단위프로젝트 c,u,r,d 기능 테스트 완료

---
+++ section08/src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css'
+import './css/App.css'
+import List from './Components/List'
+import Header from './Components/Header'
+import Editor from './Components/Editor'
+import { useState, useRef } from 'react'
+
+//전역변수
+const mockData = [ 
+  { 
+    id: 0, 
+    isDone: false, 
+    content: "React 공부하기", 
+    date: new Date().getTime(), 
+  }, 
+  { 
+    id: 1, 
+    isDone: false, 
+    content: "스프링부트 공부하기", 
+    date: new Date().getTime(), 
+  }, 
+  { 
+    id: 2, 
+    isDone: false, 
+    content: "자바 공부하기", 
+    date: new Date().getTime(), 
+  }, 
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+  //이벤트함수(setTodos 생성)
+  const onCreate = (content)=>{
+    let newTodo =  { 
+    id: idRef.current++, 
+    isDone: false, 
+    content: content, 
+    date: new Date().getTime(), 
+  }
+  setTodos([newTodo, ...todos])
+  }
+  //이벤트함수(setTodos 데이터 수정)
+  const onUpdate = (id)=>{
+    setTodos(todos.map((todo)=>{
+      return todo.id === id ? {...todo, isDone: !todo.isDone} : todo
+    }))
+  }
+  //이벤트함수(setTodos 삭제)
+  const onDelete =(id)=>{
+    setTodos(todos.filter((todo)=>{return todo.id !== id}))
+  }
+
+  return (
+    <>
+    <div className="App">
+    <Header />
+    <Editor onCreate={onCreate} />
+    <List todos={todos} onUpdate={onUpdate} />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ section08/src/Components/Editor.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from 'react';
+import '../css/Editor.css'
+const Editor = ({onCreate})=>{
+  const [content, setContent] = useState('')
+  const inputRef = useRef()
+  const onSubmit = ()=>{
+    if (content === '') {
+      inputRef.current.focus()
+      return
+    }
+    onCreate(content)
+    setContent('')
+  }
+  const onKeyDown = (e)=>{
+    if (e.keyCode === 13) {
+      onSubmit()
+    }
+    console.log(e)
+  }
+  const onChangeContent = (e)=>{
+    setContent(e.target.value)
+  }
+  return <>
+   <div className="Editor"> 
+      <input value={content} ref={inputRef} onChange={onChangeContent} onKeyDown={onKeyDown} placeholder="새로운 Todo..." /> 
+      <button onClick={onSubmit}>추가</button> 
+    </div>
+  </>
+}
+export default Editor;
